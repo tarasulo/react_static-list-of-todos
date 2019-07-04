3aee676acc403318805aedca96f173d9442522d1
Taras Khalak <tarasulo@gmail.com>
2019-07-04 13:59:47 +0300

    Updates

--- precache-manifest.e2501bbc1fc40c7ea8f86809ed7c327f.js
+++ precache-manifest.e2501bbc1fc40c7ea8f86809ed7c327f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "10c8c00e10b9900521aee9e7debcee2d",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
+    "revision": "30b491e84b1ac9d6a67c",
     "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "2315f74ce3c99316133d",
-    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-todos/static/js/2.95678282.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "30b491e84b1ac9d6a67c",
+    "url": "/react_static-list-of-todos/static/js/main.4b5b3aa6.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
